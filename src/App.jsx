95ea38ec54d1a3f0e745036fc7e0198b05a43282
murad-dabbous <moradyousef04@gmail.com>
2024-06-25 18:41:38 +0000

    Building more reusable components

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
 import Heading from "./ui/Heading";
+import Row from "./ui/Row";
 // retuning a react component
 
 // as a convention we name the parent styling for the app as StyledApp
 // we can keep this here because we don't need it anywhere else
 const StyledApp = styled.main`
-  background-color: orange;
+  /* background-color: orange; */
   padding: 20px;
 `;
 const App = () => {
@@ -16,13 +17,29 @@ const App = () => {
     <>
       <GlobalStyles />
       <StyledApp>
-        <Heading as="h1">The Wild Oasis</Heading>
-
-        <Heading as="h2">Check in and out</Heading>
-        <Button onClick={() => alert("Check in!")}>Check in</Button>
-        <Button onClick={() => alert("Check Out!")}>Check out</Button>
-        <Heading as="h3">Form</Heading>
-        <Input type="number" placeholder="Number of guests..." />
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">The Wild Oasis</Heading>
+            <div>
+              <Heading as="h2">Check in and out</Heading>
+              <Button onClick={() => alert("Check in!")}>Check in</Button>
+              <Button
+                variation="secondary"
+                size="small"
+                onClick={() => alert("Check Out!")}
+              >
+                Check out
+              </Button>
+            </div>
+          </Row>
+          <Row>
+            <Heading as="h3">Form</Heading>
+            <form>
+              <Input type="number" placeholder="Number of guests..." />
+              <Input type="number" placeholder="Number of guests..." />
+            </form>
+          </Row>
+        </Row>
       </StyledApp>
     </>
   );
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -51,20 +51,16 @@ const variations = {
 };
 
 export const Button = styled.button`
-  font-size: 1.4rem;
-  padding: 1.2rem 1.6rem;
-  font-weight: 500;
   border: none;
   /* this kind of css called design tokens */
   border-radius: var(--border-radius-sm);
-  background-color: var(--color-brand-600);
-  color: var(--color-brand-50);
   box-shadow: var(--shadow-sm);
-  cursor: pointer;
-  /* the & means for this element (the styles) */
-  &:hover {
-    background-color: var(--color-brand-700);
-  }
+  ${(props) => sizes[props.size]}
+  ${(props) => variations[props.variation]}
 `;
 
+Button.defaultProps = {
+  variation: "primary",
+  size: "medium",
+};
 export default Button;
